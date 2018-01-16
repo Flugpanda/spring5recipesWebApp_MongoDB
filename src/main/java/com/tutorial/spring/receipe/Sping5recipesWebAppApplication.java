adb221a13dba463d164e4cb3915db2dc1ef06f3a
Bastian B <bastian.braeunel@gmx.de>
2018-01-16 16:07:23 +0100

    Initial Commit

---
+++ src/main/java/com/tutorial/spring/receipe/Sping5recipesWebAppApplication.java
@@ -0,0 +1,12 @@
+package com.tutorial.spring.receipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sping5recipesWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sping5recipesWebAppApplication.class, args);
+	}
+}
