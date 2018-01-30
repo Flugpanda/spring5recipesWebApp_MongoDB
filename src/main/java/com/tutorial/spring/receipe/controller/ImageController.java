be05bc533dae29e8f08fbff715370e9f9ef26943
Bastian B <bastian.braeunel@gmx.de>
2018-01-30 13:36:38 +0100

    Refactored the ImageController to render the images from the db through Tomcat. Refactored the show.html to display the images of the recipes.
    	geändert:       src/main/java/com/tutorial/spring/receipe/controller/ImageController.java
    	geändert:       src/main/resources/templates/recipes/show.html

--- src/main/java/com/tutorial/spring/receipe/controller/ImageController.java
+++ src/main/java/com/tutorial/spring/receipe/controller/ImageController.java
@@ -1,5 +1,12 @@
 package com.tutorial.spring.receipe.controller;
 
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.tomcat.util.http.fileupload.IOUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -60,4 +67,25 @@ public class ImageController {
 		
 		return "redirect:/recipes/" + recipeId + "/show";
 	}
+	
+	/**
+	 * load the image from the database and display it via Tomcat
+	 */
+    @GetMapping("recipes/{recipeId}/image")
+    public void renderImageFromDB(@PathVariable String recipeId, HttpServletResponse response) throws IOException {
+        RecipeCommand recipeCommand = recipeService.findRecipeCommandById(Long.valueOf(recipeId));
+
+        if (recipeCommand.getImage() != null) {
+            byte[] byteArray = new byte[recipeCommand.getImage().length];
+            int i = 0;
+
+            for (Byte wrappedByte : recipeCommand.getImage()){
+                byteArray[i++] = wrappedByte; //auto unboxing
+            }
+
+            response.setContentType("image/jpeg");
+            InputStream is = new ByteArrayInputStream(byteArray);
+            IOUtils.copy(is, response.getOutputStream());
+        }
+    }
 }
